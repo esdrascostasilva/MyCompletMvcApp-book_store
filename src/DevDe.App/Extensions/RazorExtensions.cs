2c2ecee52a13ee9debdc1ff9b766017192e234c2
esdrascostasilva <esdras.cts@outlook.com>
2021-08-11 22:35:19 -0300

    View adjustments completed

---
+++ src/DevDe.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+
+namespace DevDe.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string DocumentFormatter(this RazorPage page, int typerPerson, string document)
+        {
+            return typerPerson == 1 ? Convert.ToUInt64(document).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(document).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+    }
+}
