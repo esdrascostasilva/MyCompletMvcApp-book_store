9192d14b076ef23e70b3973434056fa0cecfdd92
esdrascostasilva <esdras.cts@outlook.com>
2021-08-15 10:05:01 -0300

    Created Ajax for ZipCode fill.

--- src/DevDe.App/wwwroot/js/site.js
+++ src/DevDe.App/wwwroot/js/site.js
@@ -40,3 +40,58 @@
 
     });
 }
+
+function SearchZipCode() {
+    $(document).ready(function () {
+
+        function clear_form_zipcode() {
+            // Clean Zip Code's value form
+            $("#Address_Street").val("");
+            $("#Address_Neighborhood").val("");
+            $("#Address_City").val("");
+            $("#Address_State").val("");
+        }
+
+        // When the field zipcode lost focus
+        $("#Address_ZipCode").blur(function () {
+            // new variable "zipcode" only numbers
+            var zipcode = $(this).val().replace(/\D/g, '');
+
+            // Check if the field is valid
+            if (zipcode != "") {
+                // Regular expression to valid Zip Code
+                var validzipcode = /^[0-9]{8}$/;
+
+                // Valid the Zip Code format
+                if (validzipcode.test(zipcode)) {
+                    // fill the fields when consult webservice
+                    $("#Address_Street").val("...");
+                    $("#Address_Neighborhood").val("...");
+                    $("#Address_City").val("...");
+                    $("#Address_State").val("...");
+
+                    // Consult thew webservice viacep.com.br/
+                    $.getJSON("https://viacep.com.br/ws/" + zipcode + "/json/?callback=?",
+                        function (data) {
+                            if (!("erro" in data)) {
+                                // Update the fields with consult's values
+                                $("#Address_Street").val(data.logradouro);
+                                $("#Address_Neighborhood").val(data.bairro);
+                                $("#Address_City").val(data.localidade);
+                                $("#Address_State").val(data.uf);
+                            }
+                            else {
+                                // Zip Code not found
+                                clear_form_zipcode();
+                                alert("CEP não encontrado");
+                            }
+                        });
+                }
+                else {
+                    clear_form_zipcode();
+                }
+            }
+        });
+    });
+    
+}
